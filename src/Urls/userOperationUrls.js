aff7c23b2c967e5eb7f9e266874e2adf2edef18c
PavanKumarM <pavan.muntha@intense.in>
2021-04-17 21:36:29 +0530

    f

--- src/Urls/userOperationUrls.js
+++ src/Urls/userOperationUrls.js
@@ -1,8 +1,9 @@
-const LOGIN_URL = "http://localhost:3001/login";
-const SIGN_UP_URL = "http://localhost:3001/signup";
-const ADD_TO_HISTORY = "http://localhost:3001/addToHistory";
-const GET_HISTORY = "http://localhost:3001/getHistory";
-const REMOVE_FROM_HISTORY = "http://localhost:3001/removeFromHistory";
+const LOGIN_URL = "https://stormy-island-99926.herokuapp.com/login";
+const SIGN_UP_URL = "https://stormy-island-99926.herokuapp.com/signup";
+const ADD_TO_HISTORY = "https://stormy-island-99926.herokuapp.com/addToHistory";
+const GET_HISTORY = "https://stormy-island-99926.herokuapp.com/getHistory";
+const REMOVE_FROM_HISTORY =
+  "https://stormy-island-99926.herokuapp.com/removeFromHistory";
 export {
   LOGIN_URL,
   SIGN_UP_URL,
